ee0de492f8f1da9c7f681afd7fc2cd3d3ed048db
shnag4707 <shnag4707@HRH-IC0027341.wrdsb.ca>
2018-02-12 11:22:05 -0500

    create A1Q1

---
+++ src/Assignment1/A1Q1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment1;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+import becker.robots.Wall;
+import java.awt.Color;
+
+/**
+ *
+ * @author shnag4707
+ */
+public class A1Q1 {
+    private static Direction Direction;
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+        
+        // create city for robot
+        City nashville = new City();
+                
+        // create robot
+        RobotSE wally = new RobotSE(nashville, 0, 2, Direction.WEST);
+        
+        //change robot colour
+        wally.setColor(Color.black);
+        
+        // create walls
+        new Wall (nashville, 1, 2, Direction.NORTH);
+        new Wall (nashville, 1, 1, Direction.NORTH);
+        new Wall (nashville, 1, 1, Direction.WEST);
+        new Wall (nashville, 2, 1, Direction.WEST);
+        new Wall (nashville, 2, 2, Direction.SOUTH);
+        new Wall (nashville, 2, 1, Direction.SOUTH);
+        
+                
+    }
+
+    private static void Wall(City nashville, int i, int i0, Direction direction) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
