dfe99ed09922a5c557b77637c373a5c84a6a9469
shnag4707 <shnag4707@HRH-IC0027341.wrdsb.ca>
2018-02-13 11:22:36 -0500

    create A1Q3 and 4 update A1Q2

---
+++ src/Assignment1/A1Q4.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment1;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.RobotSE;
+
+/**
+ *
+ * @author shnag4707
+ */
+public class A1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create city
+        City miami = new City();
+        
+                
+        //create robot1
+        RobotSE wally = new RobotSE(miami, 0, 0, Direction.SOUTH);
+        
+        //create robot2
+        RobotSE eve = new RobotSE(miami, 0, 1, Direction.SOUTH);
+        
+        //create walls
+        new Wall
+        
+        
+        
+        
+        
+        
+        
+        
+                
+    }
+}
